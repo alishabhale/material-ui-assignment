011d3f800e94321be325e241767c9d3f1fd144ec
alishabhale <abhale@eisinc.com>
2024-01-27 13:05:59 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -3,26 +3,23 @@ import './App.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
-import  Dashboard from './Dashboard'
-import  Orders from './Orders'
+import  Dashboard from './Dashboard';
+import  Orders from './Orders';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Dashboard />} />
-            <Route path="/orders" element={<Orders/>} />
-
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </StyledEngineProvider>
-  </React.StrictMode>
+      <StyledEngineProvider injectFirst>
+        <BrowserRouter basename="/material-ui-assignment">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/orders" element={<Orders />} />
+          </Routes>
+        </BrowserRouter>
+      </StyledEngineProvider>
+    </React.StrictMode>
   );
 }
 
